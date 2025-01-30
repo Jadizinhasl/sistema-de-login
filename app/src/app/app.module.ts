7199370cc96123855b0c2dfc4202d3c17b4219f9
Jadizinhasl <jadi6486@gmail.com>
2025-01-30 18:17:07 -0300

    feat: estilização do site

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HomeComponent } from './components/home/home.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
